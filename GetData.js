c619dd7f6de8659c6a365f4138291aacd2894192
Andre Sanches <Andre.Sanches@iquate.com>
2021-03-30 18:30:13 +0100

    test

---
+++ GetData.js
@@ -0,0 +1,35 @@
+
+async function getCoronaData2() {
+let response = await fetch("https://covid19ireland-geohive.hub.arcgis.com/");
+let data = await response.text();
+
+console.log(data);
+
+data = String(data).replaceAll("Oxford/AstraZeneca, ", "Oxford/AstraZeneca");
+data = String(data).replaceAll("Moderna, ", "Moderna");
+data = String(data).replaceAll("Pfizer/BioNTech, ", "Pfizer/BioNTech");
+
+
+
+var arr = String(data).split('\n');
+
+var jsonObj = [];
+var headers = arr[0].split(',');
+
+
+
+for(var i = 1; i < arr.length; i++) {
+  var data2 = arr[i].split(',');
+  
+  
+  
+  var obj = {};
+  for(var j = 0; j < data2.length; j++) {
+     obj[headers[j].trim()] = data2[j].trim();
+  }
+  if (obj.location != "") {  jsonObj.push(obj); }
+}
+return JSON.stringify(jsonObj);
+
+
+}

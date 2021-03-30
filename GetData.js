37952836b2960320dc08ba7af498b88ba0a3d238
Andre Sanches <Andre.Sanches@iquate.com>
2021-03-30 18:47:26 +0100

    test

--- GetData.js
+++ GetData.js
@@ -2,36 +2,12 @@
 
 
 async function getCoronaData2() {
-let response = await fetch("http://cors.io/?https://covid19ireland-geohive.hub.arcgis.com/");
-let data = await response.text();
+	
+	
+var obj = jQuery.get("https://covid19ireland-geohive.hub.arcgis.com/");
 
-console.log(data);
+let data = await obj;
 
-data = String(data).replaceAll("Oxford/AstraZeneca, ", "Oxford/AstraZeneca");
-data = String(data).replaceAll("Moderna, ", "Moderna");
-data = String(data).replaceAll("Pfizer/BioNTech, ", "Pfizer/BioNTech");
-
-
-
-var arr = String(data).split('\n');
-
-var jsonObj = [];
-var headers = arr[0].split(',');
-
-
-
-for(var i = 1; i < arr.length; i++) {
-  var data2 = arr[i].split(',');
-  
-  
-  
-  var obj = {};
-  for(var j = 0; j < data2.length; j++) {
-     obj[headers[j].trim()] = data2[j].trim();
-  }
-  if (obj.location != "") {  jsonObj.push(obj); }
-}
-return JSON.stringify(jsonObj);
-
+return data;
 
 }

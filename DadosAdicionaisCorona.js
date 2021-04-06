8f3f8318161cebd6e3543c6f08ce55c4d56570d9
Andre Sanches <sanchessls@gmail.com>
2021-04-06 18:19:13 +0100

    Update DadosAdicionaisCorona.js
    dia 3 adding

--- DadosAdicionaisCorona.js
+++ DadosAdicionaisCorona.js
@@ -7,6 +7,8 @@
 	 myList.push(JSON.parse('{"location":"Ireland","date":"2021-03-29","vaccine":"","source_url":"","total_vaccinations":"819676","people_vaccinated":"590688","people_fully_vaccinated":"228988"}'));
 	 
 	 	 myList.push(JSON.parse('{"location":"Ireland","date":"2021-03-30","vaccine":"","source_url":"","total_vaccinations":"840561","people_vaccinated":"603802","people_fully_vaccinated":"236759"}'));
+	  
+myList.push(JSON.parse('{"location":"Ireland","date":"2021-04-03","vaccine":"","source_url":"","total_vaccinations":"932324","people_vaccinated":"660800","people_fully_vaccinated":"271524"}'));	  
 	  	 
 	 //starting 
 	 

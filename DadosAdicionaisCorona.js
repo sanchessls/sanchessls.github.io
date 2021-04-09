55734689e4f57ed3e59523cc3f833a33a3b3f63c
AndreSanches <sanchessls@gmail.com>
2021-04-09 18:00:06 +0100

    ae

--- DadosAdicionaisCorona.js
+++ DadosAdicionaisCorona.js
@@ -6,12 +6,19 @@
 	  
 	 myList.push(JSON.parse('{"location":"Ireland","date":"2021-03-29","vaccine":"","source_url":"","total_vaccinations":"819676","people_vaccinated":"590688","people_fully_vaccinated":"228988"}'));
 	 
-	 	 myList.push(JSON.parse('{"location":"Ireland","date":"2021-03-30","vaccine":"","source_url":"","total_vaccinations":"840561","people_vaccinated":"603802","people_fully_vaccinated":"236759"}'));
+	 myList.push(JSON.parse('{"location":"Ireland","date":"2021-03-30","vaccine":"","source_url":"","total_vaccinations":"840561","people_vaccinated":"603802","people_fully_vaccinated":"236759"}'));
 	  
-myList.push(JSON.parse('{"location":"Ireland","date":"2021-04-03","vaccine":"","source_url":"","total_vaccinations":"932324","people_vaccinated":"660800","people_fully_vaccinated":"271524"}'));	  
-	  	 
-	 //starting 
+     myList.push(JSON.parse('{"location":"Ireland","date":"2021-04-03","vaccine":"","source_url":"","total_vaccinations":"932324","people_vaccinated":"660800","people_fully_vaccinated":"271524"}'));	  
 	 
+	 
+	 
+	 //INICIO AUTOMATICOS
+
+     //FIM AUTOMATICOS	  	 
+	 
+	 
+	 
+	 //starting 	 
 	  var objeto =  JSON.parse(data);	 	 	 	 				 			
 	 
 	  myList.forEach( (element) => {

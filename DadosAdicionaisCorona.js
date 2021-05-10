17fe188dea8c8912d9cf76cfa55c5de62296ae8c
sanchessls <sanchessls@gmail.com>
2021-05-10 11:16:26 +0100

    10-05-2021 11-16 Auao16aaico

--- DadosAdicionaisCorona.js
+++ DadosAdicionaisCorona.js
@@ -67,6 +67,8 @@
  myList.push(JSON.parse('{"location":"Ireland","date":"2021-05-06","vaccine":"","source_url":"","total_vaccinations":1746912,"people_vaccinated":1267167,"people_fully_vaccinated":479745}'));	 
 
  myList.push(JSON.parse('{"location":"Ireland","date":"2021-05-07","vaccine":"","source_url":"","total_vaccinations":1799190,"people_vaccinated":1305178,"people_fully_vaccinated":494012}'));	 
+
+ myList.push(JSON.parse('{"location":"Ireland","date":"2021-05-08","vaccine":"","source_url":"","total_vaccinations":1827610,"people_vaccinated":1327821,"people_fully_vaccinated":499789}'));	 
 //FIM AUTOMATICOS	  	 
 	 
 	}

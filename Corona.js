aa4ff551855996caaf9376c50d1a7199d1195c44
Andre Sanches <Andre.Sanches@iquate.com>
2021-07-06 10:26:39 +0100

    dias

--- Corona.js
+++ Corona.js
@@ -8,7 +8,7 @@ var formatterNumber = new Intl.NumberFormat('en-UK', {
 });
 
 //Dias Rodar
-var diasRodar = [ 1,2,3,4,5,6,7,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+var diasRodar = [ 1,2,3,4,5,6,7,10];
 //var diasRodar = [ 2];
 
 function GetMaxDias()

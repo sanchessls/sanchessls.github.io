a6bbfbabd2a4f7b8c7410df735b8c9d6af8899d7
sanchessls <sanchessls@gmail.com>
2021-07-07 14:25:06 +0100

    07-07-2021 14-25 Aupo25apico

--- DadosAdicionaisCorona.js
+++ DadosAdicionaisCorona.js
@@ -78,6 +78,8 @@
  
 
  myList.push(JSON.parse('{"location":"Ireland","date":"2021-07-05","vaccine":"","source_url":"","total_vaccinations":4423158,"people_vaccinated":2599957,"people_fully_vaccinated":1823201}'));	 
+
+ myList.push(JSON.parse('{"location":"Ireland","date":"2021-07-06","vaccine":"","source_url":"","total_vaccinations":4486999,"people_vaccinated":2613558,"people_fully_vaccinated":1873441}'));	 
 //FIM AUTOMATICOS	  	 
 
 	 

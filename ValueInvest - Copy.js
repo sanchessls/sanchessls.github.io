1168eeb785a95504d181aa92dab39eceea3b9309
Andre Sanches <Andre.Sanches@iquate.com>
2021-08-25 10:52:23 +0100

    test

---
+++ ValueInvest - Copy.js
@@ -0,0 +1,18 @@
+   //var valorPRichard = 500;
+   //var valorPSparkLiang = 746.04;
+   
+   //06/05/2021
+   
+   //var valorPRichard = 495.87;
+   //var valorPSparkLiang = 738.21;
+
+   //08/05/2021
+
+   //var valorPRichard = 500.58;
+   //var valorPSparkLiang = 736.52;
+   
+   //18/05/2021
+   var valorPRichard = 496.33;
+   var valorPSparkLiang = 714.20;
+   
+   

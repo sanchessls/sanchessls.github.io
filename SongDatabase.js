4afe30057976f5d2db8a0b9460539a5502d2f59a
Andre Sanches <Andre.Sanches@iquate.com>
2022-08-30 20:43:22 +0100

    Adding new Song

--- SongDatabase.js
+++ SongDatabase.js
@@ -10,7 +10,11 @@ var songDatabaseSTR =
     {
         "SongName":"Perfect-EdSheeran",
         "SongLyrics":"I found a love for me Oh, darling, just dive right in and follow my lead Well, I found a girl, beautiful and sweet Oh, I never knew you were the someone waiting for me 'Cause we were just kids when we fell in love Not knowing what it was I will not give you up this time But darling, just kiss me slow Your heart is all I own And in your eyes, you're holding mine Baby, I'm dancing in the dark with you between my arms Barefoot on the grass, listening to our favourite song When you said you looked a mess, I whispered underneath my breath But you heard it, darling, you look perfect tonight Well, I found a woman, stronger than anyone I know She shares my dreams, I hope that someday I'll share her home I found a love, to carry more than just my secrets To carry love, to carry children of our own We are still kids, but we're so in love Fighting against all odds I know we'll be alright this time Darling, just hold my hand Be my girl, I'll be your man I see my future in your eyes Baby, I'm dancing in the dark, with you between my arms Barefoot on the grass, listening to our favorite song When I saw you in that dress, looking so beautiful I don't deserve this, darling, you look perfect tonight Baby, I'm dancing in the dark, with you between my arms Barefoot on the grass, listening to our favorite song I have faith in what I see Now I know I have met an angel in person And she looks perfect I don't deserve this, you look perfect tonight"
-    }    
+    },
+    {
+        "SongName":"WordsFromWork",
+        "SongLyrics":"cash sales our lady's children hospital crumlin republic of ireland marketplace sallynoggin house charton swanlea park academy childcare cabinteely ballyboden primary care additional south western area health board maynooth community unit national rehabilitation ballinteer centre rathfarnham garda station clonskeagh administration st james rehab outreach team request for sites staff replacement the sunshine home cashel road adult mental services tallaght peadiatrics optamology unit and chamber drimnagh cherry orchard only pat mcauley peter trust monkstown joseph brennan bakery sick cover luke ashdale stepaside catering rathcoole beaufort, main street, depaul firhouse treatment killarney belvilla community children's elderly clondalkin ireland at rathminies marysrathmines"
+    }        
 ]
 }`;
 
